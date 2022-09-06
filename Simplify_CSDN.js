cfdcbec5bb7e7d53cfdc20cb3e5801552fac398b
BitComing <livov@foxmail.com>
2022-09-06 18:44:08 +0800

    Update局部实现没有“登陆后复制”的按钮

--- Simplify_CSDN.js
+++ Simplify_CSDN.js
@@ -2,13 +2,14 @@
 // @name         CSDN简化工具
 // @namespace    https://tampermonkey.net/
 // @homepage     https://github.com/BitComing
-// @version      1.0
+// @version      1.3
 // @description  自由复制CSDN的代码，不用登录用户
 // @author       Alpha
 // @match        https://blog.csdn.net/*/article/details/*
 // @icon         https://g.csdnimg.cn/static/logo/favicon32.ico
 // @grant        GM_addStyle
 // ==/UserScript==
+//@note 1.3：局部实现没有“登陆后复制”的按钮
 //@note 1.2：简化了清除组件的实现方式
 //@note 1.1：去除登陆弹窗（未完成）
 //@note 1.0：删除各类无关页面内容
@@ -35,6 +36,9 @@
     clean_obj.forEach(c => {
         GM_addStyle(`${c} {display: none !important}`);
     });
+    for(let i=1; i <=15; i++){
+         GM_addStyle(`#content_views > pre:nth-child(${i}) > code > div {display: none !important}`);
+    }
 
     // 自定义主内容
     GM_addStyle('#mainBox > main {float: none !important; margin: auto !important;}');
